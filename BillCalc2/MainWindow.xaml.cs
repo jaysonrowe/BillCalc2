39c2ddad5ae193a26f51d51b3489273b2f5af17a
Jayson Rowe <jayson.rowe@gmail.com>
2012-09-16 21:52:14 -0400

    initial commit

---
+++ BillCalc2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BillCalc2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, RoutedEventArgs e)
+        {
+            string starting = balanceBox.Text;
+            string rent = rentBox.Text;
+            string lights = lightBox.Text;
+            string cable = cableBox.Text;
+            string water = waterBox.Text;
+            string cell = cellBox.Text;
+            string car1 = carBox1.Text;
+            string car2 = carBox2.Text;
+            string insurance = insBox.Text;
+            string other = otherBox.Text;
+            
+            string balance = (
+                Convert.ToDecimal(starting) -
+                Convert.ToDecimal(rent) -
+                Convert.ToDecimal(lights) -
+                Convert.ToDecimal(cable) -
+                Convert.ToDecimal(water) -
+                Convert.ToDecimal(cell) -
+                Convert.ToDecimal(car1) -
+                Convert.ToDecimal(car2) -
+                Convert.ToDecimal(insurance) -
+                Convert.ToDecimal(other)).ToString();
+
+            remainingBalance.Text = "Remaining Balance: " + "$" + balance;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Simple Bill caculator - takes a beginnig balance and subtracts bills from it. Gives result in a message box.
+
+Will work in mono as well on Linux - simply clone to disk, open in MonoDevelop and build.
